73293d48a9d8eb92ce301603ffb170a86e310fe9
dannyylin <thedannylin@gmail.com>
2024-03-29 22:09:35 -0400

    allpy

--- CS213_Photos_41-testbranch/Controller/userPage.java
+++ CS213_Photos_41-testbranch/Controller/userPage.java
@@ -1,11 +1,23 @@
 package Controller;
 
+import app.photo;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
 public class userPage {
 
     @FXML
     private ListView<?> PhotoAlbumList;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    void logout(ActionEvent event) {
+        photo x = new photo();
+        x.changeScene("/view/login.fxml");
+    }
 
 }
