091b930ed8d2c290285a07f155f02e4dceeb7ea6
bunchofletters <97305426+bunchofletters@users.noreply.github.com>
2024-03-30 03:06:00 +0000

    up up

--- CS213_Photos_41-testbranch/Controller/userPage.java
+++ CS213_Photos_41-testbranch/Controller/userPage.java
@@ -1,6 +1,5 @@
 package Controller;
 
-import app.photo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,15 +8,46 @@ import javafx.scene.control.ListView;
 public class userPage {
 
     @FXML
+    private Button CreateAlbumButton;
+
+    @FXML
+    private Button DelAlbumButton;
+
+    @FXML
+    private Button OpenAlbumButton;
+
+    @FXML
     private ListView<?> PhotoAlbumList;
+
+    @FXML
+    private Button RenameAlbumButton;
 
     @FXML
     private Button logoutButton;
 
     @FXML
+    void createAlbum(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delButton(ActionEvent event) {
+
+    }
+
+    @FXML
     void logout(ActionEvent event) {
-        photo x = new photo();
-        x.changeScene("/view/login.fxml");
+
+    }
+
+    @FXML
+    void openAlbum(ActionEvent event) {
+
+    }
+
+    @FXML
+    void renameAlbum(ActionEvent event) {
+
     }
 
 }
