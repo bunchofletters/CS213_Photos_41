f55b3cf19d118e44b7e1cc1403823fc3a7e834af
bunchofletters <97305426+bunchofletters@users.noreply.github.com>
2024-03-31 02:32:15 +0000

    updated to be line with main

--- userPage.java
+++ userPage.java
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:Controller/userPage.java
-package Controller;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import app.photo;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 
-public class userPage implements Initializable {
+public class userPage {
 
     @FXML
     private Button RenameAlbumButton;
@@ -39,107 +29,13 @@ public class userPage implements Initializable {
     @FXML
     private Button OpenAlbumButton;
 
-    @FXML private TableColumn<photoAlbumList, SimpleStringProperty> AlbumName;
+    @FXML private TableColumn<photoAlbumList, String> AlbumName;
 
-    @FXML private TableColumn<photoAlbumList, SimpleIntegerProperty> EarliestPhotoDate;
+    @FXML private TableColumn<photoAlbumList, Number> EarliestPhotoDate;
 
-    @FXML private TableColumn<photoAlbumList, SimpleIntegerProperty> LatestPhotoDate;
+    @FXML private TableColumn<photoAlbumList, Number> LatestPhotoDate;
 
-    @FXML private TableColumn<photoAlbumList, SimpleIntegerProperty> NumberOfPhotos;
-
-    @FXML private TableView<photoAlbumList> table;
-
-    ObservableList<photoAlbumList> photoAlbum = FXCollections.observableArrayList(
-        new photoAlbumList("mon", 0, 0, 0),
-        new photoAlbumList("smon", 0, 0, 0)
-    );
-
-
-    @FXML
-    void createAlbum(ActionEvent event) {
-       // photoAlbumList newAlbum = new photoAlbumList(AlbumNameInput.getText(),0, 0, 0);
-       // table.getItems().add(newAlbum);
-    }
-
-    @FXML
-    void delButton(ActionEvent event) {
-
-     }
-
-    @FXML
-    void logout(ActionEvent event) {
-        photo x = new photo();
-        x.changeScene("/view/login.fxml");
-    }
-
-    @FXML
-    void openAlbum(ActionEvent event) {
-
-    }
-
-    @FXML
-    void renameAlbum(ActionEvent event) {
-        
-        }
-
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        AlbumName.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleStringProperty>("AlbumName"));
-        NumberOfPhotos.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleIntegerProperty>("NumberOfPhotos"));
-        EarliestPhotoDate.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleIntegerProperty>("EarliestPhotoDate"));
-        LatestPhotoDate.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleIntegerProperty>("LatestPhotoDate"));
-    
-        table.setItems(photoAlbum);
-    }
-
-}
-
-=======
-=======
->>>>>>> 70651fe (Reorder to comply with assignment)
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-
-public class userPage implements Initializable {
-
-    @FXML
-    private Button RenameAlbumButton;
-
-    @FXML
-    private Button logoutButton;
-
-    @FXML
-    private TextField AlbumNameInput;
-
-    @FXML
-    private Button CreateAlbumButton;
-
-    @FXML
-    private Button DelAlbumButton;
-
-    @FXML
-    private Button OpenAlbumButton;
-
-    @FXML private TableColumn<photoAlbumList, SimpleStringProperty> AlbumName;
-
-    @FXML private TableColumn<photoAlbumList, SimpleIntegerProperty> EarliestPhotoDate;
-
-    @FXML private TableColumn<photoAlbumList, SimpleIntegerProperty> LatestPhotoDate;
-
-    @FXML private TableColumn<photoAlbumList, SimpleIntegerProperty> NumberOfPhotos;
+    @FXML private TableColumn<photoAlbumList, Number> NumberOfPhotos;
 
     @FXML private TableView<photoAlbumList> table;
 
@@ -163,7 +59,7 @@ public class userPage implements Initializable {
     @FXML
     void logout(ActionEvent event) {
         Photo x = new Photo();
-        x.changeScene("/view/login.fxml");
+        x.changeScene("login.fxml");
     }
 
     @FXML
@@ -176,19 +72,13 @@ public class userPage implements Initializable {
         
         }
 
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        AlbumName.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleStringProperty>("AlbumName"));
-        NumberOfPhotos.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleIntegerProperty>("NumberOfPhotos"));
-        EarliestPhotoDate.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleIntegerProperty>("EarliestPhotoDate"));
-        LatestPhotoDate.setCellValueFactory(new PropertyValueFactory<photoAlbumList,SimpleIntegerProperty>("LatestPhotoDate"));
+    public void initialize() {
+        AlbumName.setCellValueFactory(f -> new SimpleStringProperty(f.getValue().getName()));
+        NumberOfPhotos.setCellValueFactory(f -> new SimpleIntegerProperty(f.getValue().getPhotoNum()));
+        EarliestPhotoDate.setCellValueFactory(f -> new SimpleIntegerProperty(f.getValue().getLowestDate()));
+        LatestPhotoDate.setCellValueFactory(f -> new SimpleIntegerProperty(f.getValue().getHighestDate()));
     
         table.setItems(photoAlbum);
     }
 
-<<<<<<< HEAD
 }
->>>>>>> 70651fe (Reorder to comply with assignment):userPage.java
-=======
-}
->>>>>>> 70651fe (Reorder to comply with assignment)
