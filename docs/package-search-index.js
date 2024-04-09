3f84d4cc24b0cb9837b38c06dd2d21dcd66a5a89
dannyylin <thedannylin@gmail.com>
2024-04-08 22:59:01 -0400

    javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Controller"}];updateSearchResults();
